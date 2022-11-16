f0b63e71fd7bd125601bb2c0c516fb20e03552b1
samyfontes <samuelfontes.coder@gmail.com>
2022-11-16 11:34:45 -0300

    Added datePicker on the subjects creation/updating view

--- yii2-dev/views/subject/_form.php
+++ yii2-dev/views/subject/_form.php
@@ -2,6 +2,8 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use yii\jui\DatePicker;
+use webvimark\extensions\DateRangePicker\DateRangePicker;
 
 /** @var yii\web\View $this */
 /** @var app\models\Subjects $model */
@@ -18,13 +20,31 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'price')->textInput() ?>
 
-    <?= $form->field($model, 'duration')->dropDownList([ 6 => '6', 12 => '12', ], ['prompt' => '']) ?>
+    <?= $form->field($model, 'duration')->dropDownList([6 => '6', 12 => '12',], ['prompt' => '']) ?>
 
     <?= $form->field($model, 'teacher_id')->textInput() ?>
+<!-- 
+    <= $form->field($model, 'starting_date')->textInput() ?>
+    <= $form->field($model, 'ending_date')->textInput() ?> -->
 
-    <?= $form->field($model, 'starting_date')->textInput() ?>
 
-    <?= $form->field($model, 'ending_date')->textInput() ?>
+    <!-- <= DatePicker::widget(['name' => 'starting_date']) ?> -->
+
+    <?php echo DatePicker::widget([
+        'model' => $model,
+        'name' => 'starting_date',
+        'attribute' => 'starting_date',
+        'dateFormat' => 'yyyy-MM-dd',
+    ]);
+    ?>
+
+    <?php echo DatePicker::widget([
+        'model' => $model,
+        'name' => 'ending_date',
+        'attribute' => 'ending_date',
+        'dateFormat' => 'yyyy-MM-dd',
+    ]);
+    ?>
 
     <div class="form-group">
         <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-success']) ?>
